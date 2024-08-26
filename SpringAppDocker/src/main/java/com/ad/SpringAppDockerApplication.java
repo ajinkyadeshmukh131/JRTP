631233222def1faf94fb00c85d1dd1ed114d7625
Ajinkya <Ajinkya@DESKTOP-M267QOM.mshome.net>
2024-08-26 13:07:48 +0530

    Initial Commit

---
+++ SpringAppDocker/src/main/java/com/ad/SpringAppDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppDockerApplication.class, args);
+	}
+
+}
